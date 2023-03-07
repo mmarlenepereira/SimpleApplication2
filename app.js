53890b8a19cbf41afb2f67baaa3db96b2f85136f
mmarlenepereira <45201836+mmarlenepereira@users.noreply.github.com>
2023-03-07 21:11:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
-
+var aboutRouter = require('./routes/about');
+var contactRouter = require('./routes/contact');
 var app = express();
 
 // view engine setup
@@ -17,8 +17,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+
+
+// handle about route
+// This route is already defined in the about.js route file, so remove it from here
+// app.get('/about', function(req, res) {
+//   res.render('about', { title: 'About Us' });
+// });
+
+// handle contact route
+// This route is already defined in the contact.js route file, so remove it from here
+// app.get('/contact', function(req, res) {
+//   res.render('contact', { title: 'Contact Us' });
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
