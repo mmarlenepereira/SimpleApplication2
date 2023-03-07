30537f2c6e28bff0dab82d55017cf6f5368d1910
mmarlenepereira <x22115234@student.ncirl.ie>
2023-03-07 22:54:11 +0000

    reviewed uppercase/lowercase in the test_links.js file

--- test/test_links.js
+++ test/test_links.js
@@ -4,19 +4,19 @@ const request = require('supertest');
 const app = require('../SimpleApplication2');
 
 describe('Test Links', function () {
-  it('should return 200 for the Home page', function (done) {
+  it('should return 200 for the home page', function (done) {
     request(app)
       .get('/')
       .expect(200, done);
   });
 
-  it('should return 200 for the About page', function (done) {
+  it('should return 200 for the about page', function (done) {
     request(app)
       .get('/about')
       .expect(200, done);
   });
 
-  it('should return 200 for the Contact page', function (done) {
+  it('should return 200 for the contact page', function (done) {
     request(app)
       .get('/contact')
       .expect(200, done);
